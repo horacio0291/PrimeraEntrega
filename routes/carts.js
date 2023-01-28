d9d91e205439c15144e0a97404b69d6df20106d0
Horacio Santiago Ortiz <horacio0291@gmail.com>
2023-01-28 17:04:36 -0300

    Casi lo tengo

--- routes/carts.js
+++ routes/carts.js
@@ -3,8 +3,10 @@ const fs = require("fs");
 const cartsRouter = Router();
 const CartManager = require("../../CartManager");
 const manager = new CartManager("./carts.json");
+const products = new CartManager("./products.json");
 
 let = newCart = { id: 0, products: [] };
+let = newProduct = { id: 0, quantity: 1 };
 
 cartsRouter.post("/add", async (req, res) => {
   await manager.addCart(newCart);
@@ -22,22 +24,19 @@ cartsRouter.get("/:cid", async (req, res) => {
   }
 });
 
+cartsRouter.post("/:cid/product/:pid", async (req, res) => {
+  let cid = req.params.cid;
+  let pid = req.params.pid;
+
+  let cartId = await manager.getCartById(cid);
+  let totalProducts = JSON.parse(await products.getProducts());
+
+  let productId = totalProducts.find((prod) => prod.id == pid);
+
+  cartId.products.push({ id: productId.id, quantity: 10 });
+  await manager.saveProduct()
+
+  res.send(cartId);
+});
+
 module.exports = cartsRouter;
-
-// let newId = 0;
-// let carts = [];
-
-// const autoIncrementalId = () => {
-//   newId++;
-//   return newId;
-// };
-
-// cartsRouter.post("/", (req, res) => {
-//   let newCart = { id: autoIncrementalId(), products: [] };
-//   carts.push(newCart);
-//   res.send(carts);
-// });
-
-// cartsRouter.get('/:cid', (req,res)=>{
-//     let id = req.params.cid;
-// })
